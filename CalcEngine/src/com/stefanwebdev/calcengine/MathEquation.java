3d28bbb5a449a8a5698bc4d2e33676cd2511a0a3
stefanvlahov <stefan.vlahov5@gmail.com>
2017-01-11 22:10:33 -0600

    Class Initializers and Constructors

--- CalcEngine/src/com/stefanwebdev/calcengine/MathEquation.java
+++ CalcEngine/src/com/stefanwebdev/calcengine/MathEquation.java
@@ -4,7 +4,7 @@ public class MathEquation {
 
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal() {return leftVal;}
@@ -15,6 +15,18 @@ public class MathEquation {
     public void setOpCode(char opCode) {this.opCode = opCode;}
 
     public double getResult() {return result;}
+
+    public MathEquation() {}
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
     public void execute() {
 
